06c819c78a1ecf0a252f6374cca00c9c630b46b0
zh4ui <zh4ui@users.noreply.github.com>
2019-11-19 19:40:17 +0800

    add AppServiceProvider

---
+++ UwpDocs/AppServiceProvider/Readme.md
@@ -0,0 +1,5 @@
+
+Examples from:
+
+- [Create and consume an app service](https://docs.microsoft.com/en-us/windows/uwp/launch-resume/how-to-create-and-consume-an-app-service)
+- [Convert an app service to run in the same process as its host app](https://docs.microsoft.com/en-us/windows/uwp/launch-resume/convert-app-service-in-process)
---
+++ UwpDocs/AppServiceProvider/ServiceInApp/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.AppService;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ServiceInApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+    private AppServiceConnection inventoryService;
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+      // Add the connection.
+      if (this.inventoryService == null)
+      {
+        this.inventoryService = new AppServiceConnection();
+
+        // Here, we use the app service name defined in the app service 
+        // provider's Package.appxmanifest file in the <Extension> section.
+        this.inventoryService.AppServiceName = "InProcessAppService";
+        this.inventoryService.PackageFamilyName = Windows.ApplicationModel.Package.Current.Id.FamilyName;
+
+        // Use Windows.ApplicationModel.Package.Current.Id.FamilyName 
+        // within the app service provider to get this value.
+
+        var status = await this.inventoryService.OpenAsync();
+
+        if (status != AppServiceConnectionStatus.Success)
+        {
+          textBox.Text = "Failed to connect";
+          this.inventoryService = null;
+          return;
+        }
+      }
+
+      // Call the service.
+      int idx = int.Parse(textBox.Text);
+      var message = new ValueSet();
+      message.Add("Command", "Item");
+      message.Add("ID", idx);
+      AppServiceResponse response = await this.inventoryService.SendMessageAsync(message);
+      string result = "";
+
+      if (response.Status == AppServiceResponseStatus.Success)
+      {
+        // Get the data  that the service sent to us.
+        if (response.Message["Status"] as string == "OK")
+        {
+          result = response.Message["Result"] as string;
+        }
+      }
+
+      message.Clear();
+      message.Add("Command", "Price");
+      message.Add("ID", idx);
+      response = await this.inventoryService.SendMessageAsync(message);
+
+      if (response.Status == AppServiceResponseStatus.Success)
+      {
+        // Get the data that the service sent to us.
+        if (response.Message["Status"] as string == "OK")
+        {
+          result += " : Price = " + response.Message["Result"] as string;
+        }
+      }
+
+      textBox.Text = result;
+    }
+
+  }
+}
